f377d30354d80d7edd2dcad8c6c06c23eabb4310
Joe <nastillmatic@gmail.com>
2023-12-01 17:34:10 +0900

    Section 3 - 새로운 할인 정책 적용과 문제점
    - discountPolicy 객체 생성을 하지 않아서 NullPointerException 발생

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,7 +1,6 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.repository.MemberRepository;
 import hello.core.repository.MemoryMemberRepository;
@@ -9,7 +8,9 @@ import hello.core.repository.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private DiscountPolicy discountPolicy;
+
+
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
